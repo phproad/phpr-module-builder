11b55b24fa25d6f10fdf0f2c64086f7d18059712
Sam Georges <sam@daftspunk.com>
2013-04-25 22:26:32 +1000

    Minor spacing fix

--- classes/builder_module.php
+++ classes/builder_module.php
@@ -2,7 +2,6 @@
 
 class Builder_Module extends Core_Module_Base
 {
-
 	protected function set_module_info()
 	{
 		return new Core_Module_Detail(
@@ -11,7 +10,6 @@ class Builder_Module extends Core_Module_Base
 			"PHP Road",
 			"http://phproad.com/"
 		);
-
 	}
 
 	public function build_admin_menu($menu)
@@ -21,11 +19,9 @@ class Builder_Module extends Core_Module_Base
 		$top->add_child('forms', 'Forms', 'builder/forms', 600)->permission('manage_forms');
 	}
 
-
 	public function build_admin_permissions($host)
 	{
 		$host->add_permission_field($this, 'manage_menus', 'Manage Menus', 'right')->display_as(frm_checkbox)->comment('Modify and create site menus');
 		$host->add_permission_field($this, 'manage_forms', 'Manage Forms', 'right')->display_as(frm_checkbox)->comment('Modify and create site form');
 	}
-
 }
